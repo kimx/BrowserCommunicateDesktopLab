a8142f2a3afa8a42f0b3f0658ec00135c5790e78
kimx <kimxinfo@gmail.com>
2015-08-19 13:25:13 +0800

    init

---
+++ DesktopApp/HttpLoginListener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DesktopApp
+{
+    public class HttpLoginListener
+    {
+        HttpListener _listener;
+        public void Start(string urlPrefix)
+        {
+            this._listener = new HttpListener();
+            this._listener.Prefixes.Add(urlPrefix);
+            this._listener.Start();
+            this._listener.BeginGetContext(new AsyncCallback(ListenerCallback), null);
+        }
+        public void ListenerCallback(IAsyncResult result)
+        {
+            if (_listener == null)
+                return;
+            HttpListenerContext context = _listener.EndGetContext(result);
+            try
+            {
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+                string responseString = string.Format("\"{0}\"", MainWindow.CurrentAccount) ;
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "application/json";
+                response.AddHeader("Access-Control-Allow-Origin", "*");//Cross site setting 。
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                //繼續接收通知
+                this._listener.BeginGetContext(new AsyncCallback(ListenerCallback), this._listener);
+            }
+
+        }
+
+        public void Close()
+        {
+            this._listener.Close();
+            this._listener = null;
+        }
+    }
+}
---
+++ DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string CurrentAccount;
+        HttpLoginListener _httpLoginListener;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.gdDisplay.IsEnabled = false;
+            this._httpLoginListener = new HttpLoginListener();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            this.lblWelcome.Content = string.Format("Welcome {0} !", txtAccount.Text);
+            CurrentAccount = txtAccount.Text;
+            this.gdDisplay.IsEnabled = true;
+            this.gdLogin.IsEnabled = false;
+            this._httpLoginListener.Start("http://localhost:60099/");
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            this.lblWelcome.Content = "";
+            this.gdDisplay.IsEnabled = false;
+            this.gdLogin.IsEnabled = true;
+            this._httpLoginListener.Close();
+        }
+    }
+}
